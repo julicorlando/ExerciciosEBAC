299381b623d953b014f3dde5ab9870e541d7af10
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-11 22:29:09 -0300

    feat: adiciona ViewSets, rotas e testes de CRUD

--- modulo13_bookstore/bookstore/urls.py
+++ modulo13_bookstore/bookstore/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from .views import HealthView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/health/", HealthView.as_view(), name="health"),
+    path("api/", include("store.urls")),
 ]
