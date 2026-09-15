7e64edad93cad952953c59f09b83e57866e3a9af
Júlio Orlando <falecomjulioorlando@outlook.com>
2026-09-15 12:43:17 -0300

    deploy: aplica migrations automaticamente no runtime da Vercel

--- modulo13_bookstore/bookstore/wsgi.py
+++ modulo13_bookstore/bookstore/wsgi.py
@@ -5,3 +5,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstore.settings")
 
 application = get_wsgi_application()
+
+if os.getenv("VERCEL") == "1":
+    from django.core.management import call_command
+
+    call_command("migrate", interactive=False, verbosity=0)
